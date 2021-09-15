1197d27115f7cff74aa1225c7606f59f1e8e38ab
hyoguen <ohg89416@gmail.com>
2021-09-15 15:05:51 +0900

    test123

--- app/src/main/java/com/example/mydestiny/MainActivity.java
+++ app/src/main/java/com/example/mydestiny/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
 Button start;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/mydestiny/StartPage.java
+++ app/src/main/java/com/example/mydestiny/StartPage.java
@@ -1,13 +1,13 @@
 package com.example.mydestiny;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
+
+
 
 public class StartPage extends AppCompatActivity {
 
